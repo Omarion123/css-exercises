722f05c158359cf476060325db2e7825e79fec4d
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 <anzymen@gmail.com>
2023-06-26 18:24:58 +0200

    learn about prefix and postfx

--- learn-javascript/javasript.js
+++ learn-javascript/javasript.js
@@ -12,10 +12,32 @@ let result = x+y;
 console.log(result);
 document.getElementById("demo").innerHTML 
 ="the result is: "+result;//first assignment
-
+/*second*/
+let t=1+8+6+3+5+7;
+document.getElementById("demo1").innerHTML=t;
+/*third*/
 let q=4;
 let w=6;
 let e=9;
 let r=77;
 let solution= (q+w+e)/r;
 document.getElementById("getem").innerHTML=solution;
+/*forth */
+let a=10;
+let b = 7 * a
+console.log(a);
+console.log(b);
+/*fifth*/
+const max=57;
+const actual=max-13;
+const percentage=actual/max;
+console.log(percentage);
+/*my birthday */
+let age=23;
+let isMyBirthday=age;
+if(isMyBirthday){
+    age++;
+}
+if(isMyBirthday){
+    ++age;
+}//all of this doesn't change anything
