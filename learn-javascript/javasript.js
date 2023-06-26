65ba525bd117922d69d53878a703d579106df810
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 <anzymen@gmail.com>
2023-06-26 17:16:51 +0200

    started learning about javascript

---
+++ learn-javascript/javasript.js
@@ -0,0 +1,12 @@
+//alert("The file is working");
+/*let x=5;
+let y=11;
+let z=x+y;*/
+//document.getElementById("demo").innerHTML="Omar";
+//console.log(z);
+//alert(z);
+var x = 10;
+var y = 20;
+var k = "30";
+let result = x+y+k;
+document.getElementById("demo").innerHTML ="the result is: "+result;
